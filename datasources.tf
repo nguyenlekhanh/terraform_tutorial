d0ed16d3734a815f657c88d42996e1d0e52c575f
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-10 17:25:06 -0600

    create key pair

--- datasources.tf
+++ datasources.tf
@@ -2,11 +2,19 @@
 #099720109477
 
 data "aws_ami" "server_ami" {
-    most_recent = true
-    owners = ["099720109477"]
+  most_recent = true
+  owners      = ["099720109477"]
 
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+#create key
+#ssh-keygen -t ed25519
+
+resource "aws_key_pair" "main_auth" {
+  key_name   = "mainkey"
+  public_key = file("~/.ssh/mainkey.pub")
 }
