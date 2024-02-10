61dbc1b1f19f94853fe18b6eebde7d3541100686
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-10 16:10:08 -0600

    create subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#terraform version = "5.36.0"
+
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
@@ -8,3 +10,13 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+resource "aws_subnet" "main_public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
