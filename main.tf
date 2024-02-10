4e9bb471b84aa214d6e8eae6922173bb455ca7a2
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-10 16:18:39 -0600

    create route table and default route

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ resource "aws_internet_gateway" "main_internet_gateway" {
   }
 }
 
+resource "aws_route_table" "main_public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.main_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+}
