d72dd7970b7057040fc3cdcb0897faf4b0083efa
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-10 17:38:31 -0600

    create ec2 instance with key pair and userdata.tpl

--- datasources.tf
+++ datasources.tf
@@ -10,11 +10,3 @@ data "aws_ami" "server_ami" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
-
-#create key
-#ssh-keygen -t ed25519
-
-resource "aws_key_pair" "main_auth" {
-  key_name   = "mainkey"
-  public_key = file("~/.ssh/mainkey.pub")
-}
--- main.tf
+++ main.tf
@@ -67,3 +67,33 @@ resource "aws_security_group" "main_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+#create key
+#ssh-keygen -t ed25519
+
+resource "aws_key_pair" "main_auth" {
+  key_name   = "mainkey"
+  public_key = file("~/.ssh/mainkey.pub")
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.main_auth.id
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  subnet_id              = aws_subnet.main_public_subnet.id
+  #user_data = file("userdata.tpl")
+  user_data = file("userdata1.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+
+
+}
