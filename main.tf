f62c19bacf63eb13d0f4a59d34f55c1e2c12980e
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-10 16:13:24 -0600

    create internet gateway

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ resource "aws_subnet" "main_public_subnet" {
     Name = "dev-public"
   }
 }
+
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
